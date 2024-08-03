e05e15c539bed8cb8e2ddcc0b1d30e90d25a94cc
Vaibhav Verma <vaibhavverma9595@gmail.com>
2024-08-03 12:47:25 +0530

    new file of intarface

---
+++ interface_with_multipleInher.java
@@ -0,0 +1,34 @@
+/**
+ * interface_with_multipleInher
+ */
+public class interface_with_multipleInher {
+    public static void main(String[] args) {
+        Queen q=new Queen();
+        q.move();
+        Hathi h=new Hathi();
+        h.move();    
+    }
+    
+}
+
+interface ChessPlayer{
+    void move();
+}
+
+class Queen implements ChessPlayer{
+    public void move(){
+        System.out.println("its queen");
+    }
+}
+
+class King implements ChessPlayer{
+    public void move(){
+        System.out.println("its kinggg");
+    }
+}
+
+class Hathi implements ChessPlayer{
+    public void move(){
+        System.out.println("its hati");
+    }
+}
