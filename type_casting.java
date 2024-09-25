ec1811be47ad33d75f7d9a15c2e3dfeb692cbf89
Vaibhav Verma <134628720+iamviratvaibhav@users.noreply.github.com>
2024-09-25 22:26:34 +0530

    Add files via upload

---
+++ type_casting.java
@@ -0,0 +1,17 @@
+public class type_casting {
+    public static void main(String[] args) {
+        //wideing (Implicit)
+    int n=10;
+    System.out.println(n);
+    double d=n;
+    System.out.println(d);
+
+        //Narrowing (explicit)
+        double data=10.556;
+        System.out.println(data);
+        int data2=(int)data;
+        System.out.println(data2);
+    }
+
+
+}
