c4fd955dc7de73a54b3fb419933080f353ba1854
Vaibhav Verma <134628720+iamviratvaibhav@users.noreply.github.com>
2024-09-25 22:24:00 +0530

    Add files via upload

---
+++ final_keyword.java
@@ -0,0 +1,12 @@
+public class final_keyword {
+    public static void main(String[] args) {
+        FinalDemo fd=new FinalDemo();
+        fd.m1();
+    }
+}
+final class FinalDemo{
+    final int n=10;
+    final void m1(){
+        System.out.println("Virat Verma");
+    }
+}
